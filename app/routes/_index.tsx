e69f5375cc04c1e7ec0006811285106c41af864e
Berk Ziya <berkziya1@gmail.com>
2024-07-19 16:50:59 +0300

    should work

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,10 @@
 import { json, LoaderFunction, MetaFunction } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
+import { MovieBox } from '~/components/MovieBox';
 
 export const loader: LoaderFunction = async ({ context }) => {
   const { env } = context.cloudflare;
-  const response = await fetch('https://api.themoviedb.org/3/tv/popular', {
+  const response = await fetch('https://api.themoviedb.org/3/movie/popular', {
     headers: {
       Authorization: `Bearer ${env.TMDB_Token}`,
     },
@@ -13,13 +14,7 @@ export const loader: LoaderFunction = async ({ context }) => {
 };
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: 'New Remix App' },
-    {
-      name: 'description',
-      content: 'Welcome to Remix on Cloudflare Workers!',
-    },
-  ];
+  return [{ title: 'Muvi' }];
 };
 
 export default function Index() {
@@ -30,17 +25,7 @@ export default function Index() {
       {
         // @ts-expect-error data is not typed
         data.results.map((movie) => (
-          <div key={movie.id} className='flex items-center space-x-4'>
-            <img
-              src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-              alt={movie.name}
-              className='w-12 h-12 rounded-lg'
-            />
-            <div>
-              <h2 className='text-lg font-semibold'>{movie.name}</h2>
-              <p className='text-sm'>{movie.overview}</p>
-            </div>
-          </div>
+          <MovieBox {...movie} />
         ))
       }
     </div>
