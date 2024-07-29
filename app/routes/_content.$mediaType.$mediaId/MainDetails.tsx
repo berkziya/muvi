9a50981ea4bc8d7eaa04d65e02e05e9e19414c68
berk ziya <berkziya1@gmail.com>
2024-07-29 12:26:41 +0300

    refactor: Update MainDetails component styling

--- app/routes/_content.$mediaType.$mediaId/MainDetails.tsx
+++ app/routes/_content.$mediaType.$mediaId/MainDetails.tsx
@@ -1,6 +1,6 @@
 export function MainDetails(response: any) {
   return (
-    <div className='flex flex-row items-start mb-6'>
+    <div className='flex flex-row items-start mt-2 mb-6'>
       <div className='h-48 md:h-60 lg:h-72 aspect-[2/3]'>
         <img
           src={`https://image.tmdb.org/t/p/w500${response.poster_path}`}
@@ -8,7 +8,7 @@ export function MainDetails(response: any) {
         />
       </div>
       <div className='p-2'>
-        <h1 className='pt-7 text-l font-semibold text-accent-300'>
+        <h1 className='text-l font-semibold text-accent-300'>
           {response.title ?? response.name}
         </h1>
         <p className='text-md line-clamp-6 hover:line-clamp-none'>
